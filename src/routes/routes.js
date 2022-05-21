e9ac27a3e98b05a38294de2ff8bd0fac8e158c74
Viviane Brenner <brenner.vivi@gmail.com>
2022-05-20 22:20:49 -0300

    Preparando revisão de código

--- src/routes/routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import userRoutes from './userRoutes.js';
-import balanceRoutes from './balanceRoutes.js';
-
-const router = express.Router();
-router.use(userRoutes);
-router.use(balanceRoutes);
-export default router;
